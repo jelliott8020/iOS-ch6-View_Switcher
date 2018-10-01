6ccc19587b69bf69a88935912846cd547f89032f
Josh Elliott <josh@joshs-macbook-pro.local>
2018-10-01 17:27:34 -0400

    Finished Ch6

---
+++ ViewSwitcher/SwitchingViewController.swift
@@ -0,0 +1,105 @@
+//
+//  SwitchingViewController.swift
+//  ViewSwitcher
+//
+//  Created by Josh Elliott on 10/1/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import UIKit
+
+class SwitchingViewController: UIViewController {
+    
+    private var blueViewController: BlueViewController!
+    private var yellowViewController: YellowViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        blueViewController =
+            storyboard?.instantiateViewController(withIdentifier: "Blue")
+            as! BlueViewController
+        blueViewController.view.frame = view.frame
+        switchViewController(from: nil, to: blueViewController)
+    }
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        //Create the new view controller, if required
+        if yellowViewController?.view.superview == nil {
+            if yellowViewController == nil {
+                yellowViewController =
+                    storyboard?.instantiateViewController(withIdentifier: "Yellow")
+                    as! YellowViewController
+            }
+        } else if blueViewController?.view.superview == nil {
+            if blueViewController == nil {
+                blueViewController =
+                    storyboard?.instantiateViewController(withIdentifier: "Blue")
+                    as! BlueViewController
+            }
+        }
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        //Switch view controllers
+        if blueViewController != nil
+            && blueViewController!.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            yellowViewController.view.frame = view.frame
+            switchViewController(from: blueViewController,
+                                 to: yellowViewController)
+        } else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            blueViewController.view.frame = view.frame
+            switchViewController(from: yellowViewController,
+                                 to: blueViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+        
+        if blueViewController != nil
+            && blueViewController!.view.superview == nil {
+            blueViewController = nil
+        }
+        if yellowViewController != nil
+            && yellowViewController!.view.superview == nil {
+            yellowViewController = nil
+        }
+    }
+    
+    private func switchViewController(from fromVC:UIViewController?,
+                                      to toVC:UIViewController?) {
+        if fromVC != nil {
+            fromVC!.willMove(toParent: nil)
+            fromVC!.view.removeFromSuperview()
+            fromVC!.removeFromParent()
+        }
+        
+        if toVC != nil {
+            self.addChild(toVC!)
+            self.view.insertSubview(toVC!.view, at: 0)
+            toVC!.didMove(toParent: self)
+        }
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewSwitcher/YellowViewController.swift
@@ -0,0 +1,40 @@
+//
+//  YellowViewController.swift
+//  ViewSwitcher
+//
+//  Created by Josh Elliott on 10/1/18.
+//  Copyright © 2018 JoshElliott. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yellowButtonPressed(sender: UIButton) {
+        let alert = UIAlertController(title: "Yellow View Button Pressed",
+                                      message: "You pressed the button on the yellow view",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes, I did", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
